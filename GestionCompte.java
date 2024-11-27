14cc01dfda2bb09590b7cc659505e3a6aeb572b7
Aymen7Mg <maatougaymane@gmail.com>
2024-11-27 20:35:04 +0100

    Add files via upload

---
+++ GestionCompte.java
@@ -0,0 +1,13 @@
+package a;
+
+public class GestionCompte {
+	public static void main(String[] args) {
+        Compte compte = new Compte(500);
+
+        try {
+            compte.retirer(600);
+        } catch (SoldeInsuffisantException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
